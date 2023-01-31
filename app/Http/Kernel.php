c47e82000b27e30f37268eaf59134df031e94eaa
meshu-dev <harmeshuppal@gmail.com>
2023-01-31 20:27:50 +0000

    Cors fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,8 @@ class Kernel extends HttpKernel
 
         'api' => [
             \App\Http\Middleware\AddAuthToken::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
